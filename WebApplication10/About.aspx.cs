c1b2d6e8108ad561f8b8cfd06c9c270541638a45
Sumant Das <sumant.das@gemalto.com>
2020-01-09 11:44:11 +0530

    2 oidc tenants in 1 app
    2 oidc tenants in 1 app and redirect based on uri

---
+++ WebApplication10/About.aspx.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication10
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {           
+        }
+    }
+}
---
+++ WebApplication10/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication10
+{
+    public partial class _Default : Page
+    {
+        static int i = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //if (!Request.IsAuthenticated)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        HttpContext.Current.GetOwinContext().Authentication.Challenge(
+            //          new AuthenticationProperties { RedirectUri = "/" },
+            //          "extranet_sunchemical");
+            //    }
+            //    else
+            //    {
+            //        HttpContext.Current.GetOwinContext().Authentication.Challenge(
+            //          new AuthenticationProperties { RedirectUri = "/" },
+            //          "sunchemical");
+            //    }
+            //    i++;
+            //}
+        }
+    }
+}
